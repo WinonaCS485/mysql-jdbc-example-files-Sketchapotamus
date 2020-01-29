ec70cf21977d63407fef72e33080f2aa5f33dc48
Thomas Winters <tomffxi@gmail.com>
2020-01-29 14:56:16 -0600

    This is my commit

---
+++ StudentDatabase/src/database/TestJDBC.java
@@ -0,0 +1,10 @@
+package database;
+public class TestJDBC {
+
+    // This is just a helper program to test your database connection
+    public static void main(String[] args) throws Exception {
+        MySQLAccess dao = new MySQLAccess();
+        dao.readDataBase();
+    }
+
+}
